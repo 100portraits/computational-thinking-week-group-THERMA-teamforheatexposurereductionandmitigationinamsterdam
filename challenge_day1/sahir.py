cc1306e55a91e7855a7205040c1d55f158835972
tomhtlnd <tj@itiger.de>
2023-09-04 16:18:17 +0200

    updated Sahirs mistakes

--- challenge_day1/sahir.py
+++ challenge_day1/sahir.py
@@ -4,8 +4,8 @@ def character():
         return "Fridge"
 
 def p1(c1, c2, c3, c4, c5, c6):
-        return f"{c1} and {c2} arrived at Sarphatipark at 11:55am. The park was busy with shoppers from the Albert Cuypmarket and the stores at the Pijp. {c3} sat eeting a shoarma and watching the green parrots in the trees. Meanwhile, {c4} and {c5} were at the repair shop in New-West. They were being serviced under warranty. {c6} was strolling around Sarphatipark as well, taking in the sounds of nature, when suddenly the park caught fire!"
+        return f"{c1} and {c2} arrived at Sarphatipark at 11:55am. The park was busy with shoppers from the Albert Cuypmarket and the stores at the Pijp. {c3} sat eating a shoarma and watching the green parrots in the trees. Meanwhile, {c4} and {c5} were at the repair shop in New-West. They were being serviced under warranty. {c6} was strolling around Sarphatipark as well, taking in the sounds of nature, when suddenly the park caught fire!"
 def p2(c1, c2, c3, c4, c5, c6):
-        return f"Amsterdamned experienced a heatwave, with {c1} overseeing the warmth. {c2} contributed to the heat, causing some chaos. {c3} and {c4} led the resistance, aiming to restore order. {c5} thought hee had a solution. Meanwhile, {c6}, an ordinary citizen, sought relief from the heatwave."
+        return f"Amsterdamned experienced a heatwave, with {c1} overseeing the warmth. {c2} contributed to the heat, causing some chaos. {c3} and {c4} led the resistance, aiming to restore order. {c5} thought he had a solution. Meanwhile, {c6}, an ordinary citizen, sought relief from the heatwave."
 def p3(c1, c2, c3, c4, c5, c6):
-        return f"After the city's cooldown, life became quiet again. {c1} wus a distant memory. {c2} calmed down, and {c3}, along with {c4}, continued their activities. {c5}'s pretentiousness persisted, though less noticeably. {c6} enjoyed the newfound comfort, free from the oppressive heat of the past."        
+        return f"After the city's cooldown, life became quiet again. {c1} was a distant memory. {c2} calmed down, and {c3}, along with {c4}, continued their activities. {c5}'s pretentiousness persisted, though less noticeably. {c6} enjoyed the newfound comfort, free from the oppressive heat of the past."        
