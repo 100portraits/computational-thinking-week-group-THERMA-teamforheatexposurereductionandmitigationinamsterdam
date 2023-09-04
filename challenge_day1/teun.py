86375fde424b2db69fac6c4f4b54208c7d22b5ff
THeerze <teunjaya@gmail.com>
2023-09-04 15:48:12 +0200

    added arguments for names in paragraph

--- challenge_day1/teun.py
+++ challenge_day1/teun.py
@@ -1,27 +1,30 @@
 def printName():
     return "Teun"
 
-def p1():
-    return "With their plan in motion, Fridge and Mini-Fridge led the Resistance through the smoky, \
-        weed-filled streets of Amsterdamned. Sir A.C. Portable Airconditioning hovered nearby, \
-        constantly offering advice that sometimes bordered on absurd. Bob, feeling a mix of fear and exhilaration, \
+def character():
+    return "Sir A.C. Portable Airconditioning"
+
+def p1(c1, c2, c3, c4, c5, c6):
+    return "With their plan in motion, ", c3, " and ", c4, " led the Resistance through the smoky, \
+        weed-filled streets of Amsterdamned. ", c5, " hovered nearby, \
+        constantly offering advice that soemtimes bordered on absurd. ", c6, ", feeling a mix of fear and exhilaration, \
         followed their determined strides, realizing that this unlikely team was the cities' last hope. \
         As they approached the heart of the city, the canals bubbling like a cauldron, \
-        they saw the imposing figure of Mr. H. Stress overseeing the chaos. Mic 'MC' Oven stood by his side, \
+        they saw the imposing figure of ", c1, " overseeing the chaos. ", c2, " stood by his side, \
         his fiery words heating up the already sweltering atmosphere. The heroes braced themselves for the battle \
         that would decide the fate of Amsterdamned, ready to face the heat and the rhythm of their adversaries head-on."
 
-def p2():
-    return "As Fridge and Mini-Fridge pressed forward toward the final battle, Sir A.C. Portable Airconditioning prattled \
-        on about cooling tactics. Bobadmired the unlikely camaraderie that had formed among them. They had uncovered a weakness \
-        in Mr. H. Stress and Mic 'MC' Oven—their unbreakable bond. United by their love for fiery rhymes and oppressive heat, \
+def p2(c1, c2, c3, c4, c5, c6):
+    return "As ", c3, " and ", c4, " presed forward toward the final battle, ", c5, " prattled \
+        on about cooling tactics.", c6, " admired the unlikely camaraderie that had formed among them. They had uncovered a weakness \
+        in ", c1, " and ", c2, "—their unbreakable bond. United by their love for fiery rhymes and oppressive heat, \
         the villains were vulnerable. The heroes readied themselves for the showdown, each armed with determination. \
         It was time to exploit their foes' weakness and bring coolness back to Amsterdamned."
 
 
-def p3():
-    return "As the battle raged against Mr. H-Stress and Mic 'MC' Oven, Fridge and Mini-Fridge stood strong. \
-        Sir AC Portable Airconditioning dispensed advice, occasionally helpful. Bob watched in awe. Despite the villains' \
-        unbreakable bond, Fridge used their love for fiery rhymes against them. Hope simmered as the heroes faced their adversaries. \
+def p3(c1, c2, c3, c4, c5, c6):
+    return "As the battle raged against ", c1, " and ", c2, ", ", c3, " and ", c4, " stood strong. \
+        ", c5, " dispensed adviec, occasionally helpful. ", c6, " watched in awe. Despite the villains' \
+        unbreakable bond, ", c3, " used their love for fiery rhymes against them. Hope simmered as the heroes faced their adversaries. \
         In a climactic showdown, the city's fate hung in the balance. With a surge of determination, \
         the heroes confronted the blazing duo, ready to reclaim Amsterdamnd from the relentless heat."
