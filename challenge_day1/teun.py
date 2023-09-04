2ed1cba899cd0737ff369ada877fd737d4f36734
THeerze <teunjaya@gmail.com>
2023-09-04 15:30:43 +0200

    added my personal paragraphs

--- challenge_day1/teun.py
+++ challenge_day1/teun.py
@@ -1,2 +1,27 @@
 def printName():
     return "Teun"
+
+def p1():
+    return "With their plan in motion, Fridge and Mini-Fridge led the Resistance through the smoky, \
+        weed-filled streets of Amsterdamned. Sir A.C. Portable Airconditioning hovered nearby, \
+        constantly offering advice that sometimes bordered on absurd. Bob, feeling a mix of fear and exhilaration, \
+        followed their determined strides, realizing that this unlikely team was the cities' last hope. \
+        As they approached the heart of the city, the canals bubbling like a cauldron, \
+        they saw the imposing figure of Mr. H. Stress overseeing the chaos. Mic 'MC' Oven stood by his side, \
+        his fiery words heating up the already sweltering atmosphere. The heroes braced themselves for the battle \
+        that would decide the fate of Amsterdamned, ready to face the heat and the rhythm of their adversaries head-on."
+
+def p2():
+    return "As Fridge and Mini-Fridge pressed forward toward the final battle, Sir A.C. Portable Airconditioning prattled \
+        on about cooling tactics. Bobadmired the unlikely camaraderie that had formed among them. They had uncovered a weakness \
+        in Mr. H. Stress and Mic 'MC' Oven—their unbreakable bond. United by their love for fiery rhymes and oppressive heat, \
+        the villains were vulnerable. The heroes readied themselves for the showdown, each armed with determination. \
+        It was time to exploit their foes' weakness and bring coolness back to Amsterdamned."
+
+
+def p3():
+    return "As the battle raged against Mr. H-Stress and Mic 'MC' Oven, Fridge and Mini-Fridge stood strong. \
+        Sir AC Portable Airconditioning dispensed advice, occasionally helpful. Bob watched in awe. Despite the villains' \
+        unbreakable bond, Fridge used their love for fiery rhymes against them. Hope simmered as the heroes faced their adversaries. \
+        In a climactic showdown, the city's fate hung in the balance. With a surge of determination, \
+        the heroes confronted the blazing duo, ready to reclaim Amsterdamnd from the relentless heat."
