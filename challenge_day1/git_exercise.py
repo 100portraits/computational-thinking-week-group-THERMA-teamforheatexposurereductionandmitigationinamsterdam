d0e4cd201b6f7ed30530ca12d096ebde5624d667
sahir <sahirde@gmail.com>
2023-09-04 15:54:50 +0200

    a

--- challenge_day1/git_exercise.py
+++ challenge_day1/git_exercise.py
@@ -1,19 +1,16 @@
-#from Kian import name as Kian
-#from Ameer import get_name as Ameer
-#from gonzalo import name1 as Gonzalo
-#from sahir import name as Sahir
-#from teun import printName as Teun
-#from tom import printName as Tom
-#from act1 import act1
-#from act2 import act2
-#from act3 import act3
-
 import Kian
 import Ameer
 import gonzalo
 import sahir
 import teun
 import tom
+
+c1=Ameer.character()
+c2=Kian.character()
+c3=sahir.character()
+c4=gonzalo.character()
+c5=teun.character()
+c6=tom.character()
 
 def fun():
     print("This is Team THERMA - teamforheatexposurereductionandmitigationinamsterdam. We are:")
@@ -29,26 +26,26 @@ fun()
 def story():
 
     #Act 1
-    print(Kian.p1())
-    print(Ameer.p1())
-    print(gonzalo.p1())
-    print(sahir.p1())
-    print(teun.p1())
-    print(tom.p1())
+    print(Kian.p1(c1, c2, c3, c4, c5, c6))
+    print(Ameer.p1(c1, c2, c3, c4, c5, c6))
+    print(gonzalo.p1(c1, c2, c3, c4, c5, c6))
+    print(sahir.p1(c1, c2, c3, c4, c5, c6))
+    print(teun.p1(c1, c2, c3, c4, c5, c6))
+    print(tom.p1(c1, c2, c3, c4, c5, c6))
 
     #Act 2
-    print(Kian.p2())
-    print(Ameer.p2())
-    print(gonzalo.p2())
-    print(sahir.p2())
-    print(teun.p2())
-    print(tom.p2())
+    print(Kian.p2(c1, c2, c3, c4, c5, c6))
+    print(Ameer.p2(c1, c2, c3, c4, c5, c6))
+    print(gonzalo.p2(c1, c2, c3, c4, c5, c6))
+    print(sahir.p2(c1, c2, c3, c4, c5, c6))
+    print(teun.p2(c1, c2, c3, c4, c5, c6))
+    print(tom.p2(c1, c2, c3, c4, c5, c6))
 
     #Act 3
-    print(Kian.p3())
-    print(Ameer.p3())
-    print(gonzalo.p3())
-    print(sahir.p3())
-    print(teun.p3())
-    print(tom.p3())
+    print(Kian.p3(c1, c2, c3, c4, c5, c6))
+    print(Ameer.p3(c1, c2, c3, c4, c5, c6))
+    print(gonzalo.p3(c1, c2, c3, c4, c5, c6))
+    print(sahir.p3(c1, c2, c3, c4, c5, c6))
+    print(teun.p3(c1, c2, c3, c4, c5, c6))
+    print(tom.p3(c1, c2, c3, c4, c5, c6))
     
