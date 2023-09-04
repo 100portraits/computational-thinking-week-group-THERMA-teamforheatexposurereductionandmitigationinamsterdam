c4bca9cbc09a26cdf0be39a90bbc4517d7041d4a
sahir <sahirde@gmail.com>
2023-09-04 15:28:15 +0200

    a

---
+++ challenge_day1/git_exercise.py
@@ -0,0 +1,54 @@
+#from Kian import name as Kian
+#from Ameer import get_name as Ameer
+#from gonzalo import name1 as Gonzalo
+#from sahir import name as Sahir
+#from teun import printName as Teun
+#from tom import printName as Tom
+#from act1 import act1
+#from act2 import act2
+#from act3 import act3
+
+import Kian
+import Ameer
+import gonzalo
+import sahir
+import teun
+import tom
+
+def fun():
+    print("This is Team THERMA - teamforheatexposurereductionandmitigationinamsterdam. We are:")
+    print(Kian.name())
+    print(Ameer.get_name())
+    print(gonzalo.name1())
+    print(sahir.name())
+    print(teun.printName())
+    print(tom.printName())
+
+fun()
+
+def story():
+
+    #Act 1
+    print(Kian.p1())
+    print(Ameer.p1())
+    print(gonzalo.p1())
+    print(sahir.p1())
+    print(teun.p1())
+    print(tom.p1())
+
+    #Act 2
+    print(Kian.p2())
+    print(Ameer.p2())
+    print(gonzalo.p2())
+    print(sahir.p2())
+    print(teun.p2())
+    print(tom.p2())
+
+    #Act 3
+    print(Kian.p3())
+    print(Ameer.p3())
+    print(gonzalo.p3())
+    print(sahir.p3())
+    print(teun.p3())
+    print(tom.p3())
+    
--- challenge_day1/sahir.py
+++ challenge_day1/sahir.py
@@ -1,2 +1,11 @@
 def name():
 	return "Sahir"
+def character():
+        "Mr. H. Stress"
+
+def p1():
+        return "Mr. H-Stress and Mic Oven arrpved at Sarphatipark at 11:55am. The park was busy with shoppers from the Albert Cuypmarket and the stores at the Pijp. Bob sat eeting a shoarma and watching the green parrots in the trees. Meanwhile, Fridge and mini-fridge were at the repair shop in New-West. They were being serviced under warranty. Sir A.C. was strolling around Sarphatipark as well, taking in the sounds of nature, when suddenly the park caught fire!"
+def p2():
+        return "Amsterdamned experienced a heatwave, with Mr. H-Stress overseeing the warmth. Mic "MC" Oven contributed to the heat, causing soome chaos. Fridge and Mini-Fridge led the resistance, aiming to restore order. Sir A.C. Portable Airconditioning thought hee had a solution. Meanwhile, Bob, an ordinary citizen, sought relief from the heatwave."
+def p3():
+        return "After the city's cooldown, life bicame quiet again. Mr. H-Stress wus a distant memory. Mic "MC" Oven calmed down, and Fridge, along with Mini-Fridge, continued their activities. Sir A.C. Portable Airconditioning's pretentiousness persisted, though less noticeably. Bob enjoyed the newfound comfort, free from the oppressive heat of the past."        
