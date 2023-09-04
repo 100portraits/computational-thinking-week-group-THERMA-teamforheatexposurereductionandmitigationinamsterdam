79dbd00f841224f991c58c9506116a63cfddc044
tomhtlnd <tj@itiger.de>
2023-09-04 16:20:57 +0200

    fixed mistakes

--- challenge_day1/teun.py
+++ challenge_day1/teun.py
@@ -7,7 +7,7 @@ def character():
 def p1(c1, c2, c3, c4, c5, c6):
     return f"With their plan in motion, { c3 } and { c4 } led the Resistance through the smoky, \
         weed-filled streets of Amsterdamned. { c5 } hovered nearby, \
-        constantly offering advice that soemtimes bordered on absurd. { c6 }, feeling a mix of fear and exhilaration, \
+        constantly offering advice that sometimes bordered on absurd. { c6 }, feeling a mix of fear and exhilaration, \
         followed their determined strides, realizing that this unlikely team was the cities' last hope. \
         As they approached the heart of the city, the canals bubbling like a cauldron, \
         they saw the imposing figure of { c1 } overseeing the chaos. { c2 } stood by his side, \
@@ -15,7 +15,7 @@ def p1(c1, c2, c3, c4, c5, c6):
         that would decide the fate of Amsterdamned, ready to face the heat and the rhythm of their adversaries head-on."
 
 def p2(c1, c2, c3, c4, c5, c6):
-    return f"As { c3 } and { c4 } presed forward toward the final battle, { c5 } prattled \
+    return f"As { c3 } and { c4 } pressed forward toward the final battle, { c5 } prattled \
         on about cooling tactics.{ c6 } admired the unlikely camaraderie that had formed among them. They had uncovered a weakness \
         in { c1 } and { c2 }—their unbreakable bond. United by their love for fiery rhymes and oppressive heat, \
         the villains were vulnerable. The heroes readied themselves for the showdown, each armed with determination. \
@@ -24,7 +24,7 @@ def p2(c1, c2, c3, c4, c5, c6):
 
 def p3(c1, c2, c3, c4, c5, c6):
     return f"As the battle raged against { c1 } and { c2 }, { c3 } and { c4 } stood strong. \
-        { c5 } dispensed adviec, occasionally helpful. { c6 } watched in awe. Despite the villains' \
+        { c5 } dispensed advice, occasionally helpful. { c6 } watched in awe. Despite the villains' \
         unbreakable bond, { c3 } used their love for fiery rhymes against them. Hope simmered as the heroes faced their adversaries. \
         In a climactic showdown, the city's fate hung in the balance. With a surge of determination, \
         the heroes confronted the blazing duo, ready to reclaim Amsterdamnd from the relentless heat."
