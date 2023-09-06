df9cbd9156b945a2a2f67c96b58a2cf2cd42e49d
tomhtlnd <tj@itiger.de>
2023-09-06 11:27:15 +0200

    Fixed station7

--- challenge_day3/station7.py
+++ challenge_day3/station7.py
@@ -1,4 +1,5 @@
 def solution_station_7(string):
     a, b, c, d, e = 3, -1, 4, 7, 0.5
-    # ???????
-    return eval(string)
+    result = eval(string, {"a": a, "b": b, "c": c, "d": d, "e": e})
+    
+    return result
