7c8c8d539804ecd0abf51633418b2b51e483e0da
sahir <sahirde@gmail.com>
2023-09-06 11:28:23 +0200

    write code for station4

--- challenge_day3/station4.py
+++ challenge_day3/station4.py
@@ -1,3 +1,22 @@
-def solution_station_4():
-    pass
-    return
+def solution_station_4(i):
+    return is_prime(i)
+
+def is_prime(i):
+    if i <= 1:
+        return False
+    elif i <= 3:
+        return True
+    elif i % 2 == 0 or i % 3 == 0:
+        return False
+    else:
+        divisor = 5
+        while divisor * divisor <= i:
+            if i % divisor == 0 or i % (divisor + 2) == 0:
+                return False
+            divisor += 6
+        return True
+
+
+
+for i in range(0, 23):
+    print(solution_station_4(i))
